17d953f79bfbfc3a25c4eb332583dae9661c3fee
potatonupt <1741968515@qq.com>
2023-11-07 17:44:46 +0800

    跳转到历史记录页面

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
+import com.example.demo.service.UserRepository;
 import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,19 @@ import org.springframework.web.servlet.view.RedirectView;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String index(HttpSession session, Model model) {
+        // 从会话中获取用户名
+        String username = (String) session.getAttribute("username");
+        if (username != null) {
+            model.addAttribute("loggedInUsername", username);
+        }
+        return "index";
+    }
+
 
     @GetMapping("/register")
     public String registerForm(Model model) {
@@ -33,30 +49,31 @@ public class UserController {
         return "login";
     }
 
-//    @PostMapping("/login")
-//    public String loginUser(@RequestParam String username, @RequestParam String password, Model model) {
-//        try {
-//            userService.loginUser(username, password);
-//            model.addAttribute("message", "Login successful.");
-//        } catch (Exception e) {
-//            model.addAttribute("error", "Login failed: " + e.getMessage());
-//        }
-//        return "login";
-//    }
-
     @PostMapping("/login")
-    public RedirectView loginUser(@RequestParam String username, @RequestParam String password, Model model) {
+    public RedirectView loginUser(@RequestParam String username, @RequestParam String password, Model model, HttpSession session) {
         try {
             userService.loginUser(username, password);
-            // 如果登录成功，重定向到index页面
+            // 如果登录成功，将用户名保存到会话
+            session.setAttribute("username", username);
             return new RedirectView("/", true);
         } catch (Exception e) {
             model.addAttribute("error", "Login failed: " + e.getMessage());
-            // 如果登录失败，返回到login页面
             return new RedirectView("/login", true);
         }
     }
 
+    @GetMapping("/user/{username}")
+    public String userProfile(@PathVariable String username, Model model) {
+        // 根据用户名查询用户的个人信息，然后将信息传递给模板
+        User user = userRepository.findByUsername(username);
+        model.addAttribute("user", user);
+        return "user_profile"; // 渲染用户的个人资料页
+    }
+
+
+
+
+    //主页按钮跳转
     @GetMapping("/toLogin")
     public RedirectView toLogin() {
         // 如果需要在此处执行登录逻辑，请在此处添加登录逻辑
