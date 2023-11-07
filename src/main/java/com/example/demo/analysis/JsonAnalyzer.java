fe91f035a78ffe722a30a0865c203cc9395309cd
potatonupt <1741968515@qq.com>
2023-11-08 00:27:04 +0800

    跳转到历史记录页面

--- src/main/java/com/example/demo/analysis/JsonAnalyzer.java
+++ src/main/java/com/example/demo/analysis/JsonAnalyzer.java
@@ -6,10 +6,5 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class JsonAnalyzer {
 
 
-    public static double calculateAnalysisScore(int age, double score) {
-        // 这里可以根据你的分析逻辑来计算分数
-        // 例如，根据年龄和分数来决定分数
-        return age * score / 100;
-    }
 }
 
--- src/main/java/com/example/demo/controller/FileUploadController.java
+++ src/main/java/com/example/demo/controller/FileUploadController.java
@@ -43,8 +43,6 @@ public class FileUploadController
 //                        System.out.println("JSON File Content: " + jsonContent.toString());
 
                         String json = jsonContent.toString();
-//                        System.out.println(json);
-//                        String json = "{\"name\": \"John\", \"age\": 30, \"city\": \"New York\"}";
                         try
                         {
                             // 初始化 Jackson ObjectMapper
@@ -53,33 +51,37 @@ public class FileUploadController
                             // 解析 JSON 数据
                             JsonNode rootNode = objectMapper.readTree(json);
 
-                            // 获取 "targets" 节点
-                            JsonNode targetsNode = rootNode.get("targets");
-                            System.out.println(targetsNode.get(20));
-//                            if (targetsNode.isArray())
-//                            {
-//                                for (JsonNode target : targetsNode)
-//                                {
-//                                    // 获取 "History" 节点
-//                                    JsonNode historyNode = target.get("lists").get("zWw,J;#Qe7hmIOddL`xw-History-list");
-//
-//                                    if (historyNode.isArray())
-//                                    {
-//                                        for (JsonNode historyItem : historyNode)
-//                                        {
-//                                            String expression = historyItem.asText();
-//
-//                                            // 根据评分标准检查表达式
-//                                            if (expression.contains("5+9") && expression.contains("9*8-9/9"))
-//                                            {
-//                                                // 符合评分标准，可以增加分数
-//                                                // 这里可以根据评分标准定义你的评分逻辑
-//                                                System.out.println("Found a high-quality expression: " + expression);
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
+                            // 初始化计数器
+                            int totalIfStatementCount = 0;
+
+                            // 遍历每个角色
+                            JsonNode targets = rootNode.get("targets");
+                            for (JsonNode target : targets) {
+                                if (!target.has("blocks")) {
+                                    continue;
+                                }
+
+                                int ifStatementCount = 0;
+
+                                // 遍历角色的blocks字段，查找"opcode"为"control_if"的块
+                                JsonNode blocks = target.path("blocks");
+                                for (JsonNode block : blocks) {
+                                    if (block.has("opcode") && block.get("opcode").asText().equals("control_if")) {
+                                        ifStatementCount++;
+                                    }
+                                }
+
+                                // 打印每个角色的if语句数量
+                                System.out.println("角色 '" + target.get("name").asText() + "' 的if语句数量: " + ifStatementCount);
+
+                                // 累加到总计数器
+                                totalIfStatementCount += ifStatementCount;
+                            }
+
+                            // 打印总if语句的数量
+                            System.out.println("总if语句的数量: " + totalIfStatementCount);
+
+
                         }
                         catch (Exception e)
                         {
