f54deb6fdf3caad437f505b39c40cd344b3f696f
potatonupt <1741968515@qq.com>
2023-11-09 01:08:43 +0800

    成分分析

--- src/main/java/com/example/demo/analysis/JsonAnalyzer.java
+++ src/main/java/com/example/demo/analysis/JsonAnalyzer.java
@@ -7,14 +7,24 @@ public class JsonAnalyzer
 {
     public static int[] analyzeSb3Project(String json)
     {
-        int[] results = new int[2];  // 创建一个包含两个整数的数组，用于存储结果
+        int[] results = new int[20];  // 创建一个包含两个整数的数组，用于存储结果
 
         //if
         int ifStatementCount = 0;
         int totalIfStatementCount = 0;
 
-        int motionNumber = 0;
-
+        int motionCount = 0;
+        int looksCount = 0;
+        int soundCount = 0;
+        int eventCount = 0;
+        int controlCount = 0;
+        int sensingCount = 0;
+        int operatorCount = 0;
+        int dataCount = 0;
+        int proceduresCount = 0;
+        int penCount = 0;
+        int canvasCount = 0;
+        int totalcodeCount=0;
         try
         {
             // 初始化 Jackson ObjectMapper
@@ -36,22 +46,62 @@ public class JsonAnalyzer
                 JsonNode blocks = target.path("blocks");
                 for (JsonNode block : blocks)
                 {
-                    if (block.has("opcode") && block.get("opcode").asText().equals("control_if"))
+                    if (block.has("opcode"))
                     {
-                        ifStatementCount++;
-                    }
-                    if (block.has("opcode") && block.get("opcode").asText().startsWith("motion_"))
-                    {
-                        motionNumber++;
+
+                        totalcodeCount++;
+//                        if (block.get("opcode").asText().equals("control_if"))
+//                        {
+//                            ifStatementCount++;
+//                        }
+                        if (block.get("opcode").asText().startsWith("motion_"))
+                        {
+                            motionCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("looks_"))
+                        {
+                            looksCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("sound_"))
+                        {
+                            soundCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("event_"))
+                        {
+                            eventCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("control_"))
+                        {
+                            controlCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("sensing_"))
+                        {
+                            sensingCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("operator_"))
+                        {
+                            operatorCount++;
+                        }
+                        if (block.get("opcode").asText().startsWith("procedures_"))
+                        {
+                            proceduresCount++;
+                        }
                     }
                 }
 
-                // 累加到总计数器
-                totalIfStatementCount += ifStatementCount;
             }
 
-            results[0] = totalIfStatementCount;
-            results[1] = motionNumber;
+            results[0] = totalcodeCount;
+            results[1] = motionCount;
+            results[2] = looksCount;
+            results[3] = soundCount;
+            results[4] = eventCount;
+            results[5] = controlCount;
+            results[6] = sensingCount;
+            results[7] = operatorCount;
+            results[8] = proceduresCount;
+
+
         }
         catch (Exception e)
         {
